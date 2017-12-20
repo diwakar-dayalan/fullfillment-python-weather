b5bf1d234d3d99cf9f4ff2e405709df44da23ba1
diwakar-dayalan <31245932+diwakar-dayalan@users.noreply.github.com>
2017-12-20 14:47:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,17 +52,27 @@ def webhook():
 def processRequest(req):
     
      ##added the below 4 lines only - 12/19
-    if req.get("result").get("action") == "PreviousContext":
+    if req.get("result").get("action") == "FicoContext":
         result = req.get("result")
         parameters = result.get("parameters")
         city = parameters.get("geo-city")
         bankname = parameters.get("bank-name") 
-        fico_score = {'Federal Bank':'670','Andhra Bank': '730'}         
+        fico_score = {'Federal Bank':'649','Andhra Bank': '749'}         
         fico_str = "FICO score for " + bankname + " is " + str(fico_score[bankname])
         bankname = fico_str
         res = makeWebhookResult1(fico_str)
         return res
-        
+    
+    if req.get("result").get("action") == "CreditContext":
+        result = req.get("result")
+        parameters = result.get("parameters")
+        city = parameters.get("geo-city")
+        bankname = parameters.get("bank-name") 
+        credit_score = {'Federal Bank':'549','Andhra Bank': '730'}         
+        credit_str = "Credit Score " + bankname + " is " + str(credit_score[bankname])
+        bankname = credit_str
+        res = makeWebhookResult1(credit_str)
+        return res
     
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
