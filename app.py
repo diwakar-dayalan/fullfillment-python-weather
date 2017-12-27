9223f7b12d9a31753f3b6d4b555c5eb80834d13a
diwakar-dayalan <31245932+diwakar-dayalan@users.noreply.github.com>
2017-12-27 15:32:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,8 @@ def processRequest(req):
         roof_s ={'India Bazaar':'20','Walter B Allen': '15','Ames Construction':'6'}
         hail_s ={'India Bazaar':'20','Walter B Allen': '15','Ames Construction':'6'}
         fire_s ={'India Bazaar':'20','Walter B Allen': '15','Ames Construction':'6'}
-        fico_str = "Property Risk for " + bankname + " is \n" + "Latitude   : " + str(lat_s[bankname]) + "\n"  + "Longitude   : " + str(lon_s[bankname])+ "Roof Age    : " + str(roof_s[bankname])" years " + "Distance of Property from Firestation   : " + str(fico_score[bankname])" mins "+ "Earth Quake Risk  : " + str(earth_s[bankname])" counts in last 15 years" + "Flood Risk  : " + str(fld_s[bankname])+ "Hail  Risk  : " + str(hail_s[bankname]) + "Fire  Risk  : " + str(fire_s[bankname])         
+        #fico_str = "Property Risk for " + bankname + " is \n" + "Latitude   : " + str(lat_s[bankname]) + "\n"  + "Longitude   : " + str(lon_s[bankname])+ "Roof Age    : " + str(roof_s[bankname])" years " + "Distance of Property from Firestation   : " + str(fico_score[bankname])" mins "+ "Earth Quake Risk  : " + str(earth_s[bankname])" counts in last 15 years" + "Flood Risk  : " + str(fld_s[bankname])+ "Hail  Risk  : " + str(hail_s[bankname]) + "Fire  Risk  : " + str(fire_s[bankname])         
+        fico_str = "Property Risk for " + bankname + " is \n" + "Latitude   : " + str(lat_s[bankname]) + "\n"  + "Longitude   : " + str(lon_s[bankname])
         bankname = fico_str
         res = makeWebhookResult1(fico_str)
         return res
